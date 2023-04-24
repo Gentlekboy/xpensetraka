26584b04631b17331b28e82b8aa5f23e0693b64e
gentlekboy <gentlekboy@gmail.com>
2023-04-24 19:44:43 +0100

    refactor styles

--- app/navigation/tab/RootTab.tsx
+++ app/navigation/tab/RootTab.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
+import {View} from "react-native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {RootTabParamList} from "../types/rootTab/RootTabParamList";
 import Home from "../../screens/Home";
@@ -15,6 +15,7 @@ import BudgetInactive from "../../assets/svg/budgetInactive.svg";
 import ProfileActive from "../../assets/svg/profileActive.svg";
 import ProfileInactive from "../../assets/svg/profileInactive.svg";
 import FloatingActionButton from "../../components/fab/FloatingActionButton";
+import {styles} from "./styles";
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
@@ -89,85 +90,6 @@ const RootTab = () => {
       />
     </Tab.Navigator>
   );
-  // return (
-  //   <Tab.Navigator
-  //     initialRouteName="Home"
-  //     screenOptions={({route}) => ({
-  //       tabBarIcon: ({focused}) => {
-  //         const width = focused ? 22 : 18;
-  //         const height = focused ? 22 : 18;
-
-  //         switch (route.name) {
-  //           case 'Home':
-  //             return focused ? (
-  //               <HomeActive width={width} height={height} />
-  //             ) : (
-  //               <HomeInactive width={width} height={height} />
-  //             );
-
-  //           case 'Transactions':
-  //             return focused ? (
-  //               <TransactionActive width={width} height={height} />
-  //             ) : (
-  //               <TransactionInactive width={width} height={height} />
-  //             );
-
-  //           case 'Budget':
-  //             return focused ? (
-  //               <BudgetActive width={width} height={height} />
-  //             ) : (
-  //               <BudgetInactive width={width} height={height} />
-  //             );
-
-  //           default:
-  //             return focused ? (
-  //               <ProfileActive width={width} height={height} />
-  //             ) : (
-  //               <ProfileInactive width={width} height={height} />
-  //             );
-  //         }
-  //       },
-  //       tabBarActiveTintColor: '#7F3DFF',
-  //       tabBarInactiveTintColor: '#C6C6C6',
-  //       tabBarLabelStyle: styles.tabBarLabelStyle,
-  //       headerShown: false,
-  //     })}>
-  //     <Tab.Screen name="Home" component={Home} />
-
-  //     <Tab.Screen name="Transactions" component={Transactions} />
-
-  //     <Tab.Screen name="Budget" component={Budget} />
-
-  //     <Tab.Screen name="Profile" component={Profile} />
-  //   </Tab.Navigator>
-  // );
 };
 
 export default RootTab;
-
-const styles = StyleSheet.create({
-  tabBar: {
-    position: "absolute",
-    left: 16,
-    right: 16,
-    bottom: 8,
-    height: 56,
-    borderRadius: 16,
-    borderTopColor: "transparent",
-    backgroundColor: "#FFF",
-    shadowColor: "#000",
-    shadowOffset: {
-      height: 6,
-      width: 0,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
-    elevation: 3,
-  },
-  tabBarIconContainer: {
-    position: "absolute",
-    top: 12,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- app/navigation/tab/styles.tsx
+++ app/navigation/tab/styles.tsx
@@ -1,10 +1,28 @@
-import {StyleSheet} from 'react-native';
-import {poppins500} from '../../utils/fonts/poppins';
+import {StyleSheet} from "react-native";
 
 export const styles = StyleSheet.create({
-  tabBarLabelStyle: {
-    fontSize: 12,
-    fontFamily: poppins500,
-    marginBottom: 2,
+  tabBar: {
+    position: "absolute",
+    left: 16,
+    right: 16,
+    bottom: 8,
+    height: 56,
+    borderRadius: 16,
+    borderTopColor: "transparent",
+    backgroundColor: "#FFF",
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 6,
+      width: 0,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+  tabBarIconContainer: {
+    position: "absolute",
+    top: 12,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
